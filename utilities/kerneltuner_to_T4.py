37442c602f9ca284faa9a3d5084f9955ce36a867
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2025-03-13 19:05:37 +0100

    Adjusted script for new cachefiles location

--- utilities/kerneltuner_to_T4.py
+++ utilities/kerneltuner_to_T4.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from kernel_tuner.cache.cli_tools import convert_t4
 
 basepath = Path(__file__).parent.parent / "cachefiles"
-directories = ["convolution_milo", "dedisp_milo", "gemm_milo", "hotspot_milo"]
+directories = ["convolution_milo", "dedispersion_milo", "gemm_milo", "hotspot_milo"]
 
 for directory in directories:
     print(f"Converting files in {directory}")
